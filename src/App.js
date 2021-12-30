d7f60080061db6ca192c43427da795b0aeef3ccb
jewoo <jeus0630@gmail.com>
2021-12-30 12:21:55 -0500

    Nav Styling

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import Login from "./components/Login";
+import Header from "./components/Header";
 
 function App() {
     return (
         <div className="App">
             <Router>
+                <Header></Header>
                 <Routes>
                     <Route path="/" element={<Login></Login>}></Route>
                 </Routes>
---
+++ src/components/Header.js
@@ -0,0 +1,114 @@
+import styled from "styled-components";
+import {Link} from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <Nav>
+            <Logo>
+                <img src="/images/logo.svg" alt=""/>
+            </Logo>
+            <NavMenu>
+                <Link to="/home">
+                    <img src="/images/home-icon.svg" alt=""/>
+                    <span>HOME</span>
+                </Link>
+                <Link to="/search">
+                    <img src="/images/search-icon.svg" alt=""/>
+                    <span>SEARCH</span>
+                </Link>
+                <Link to="/watch-list">
+                    <img src="/images/watchlist-icon.svg" alt=""/>
+                    <span>WATCHLIST</span>
+                </Link>
+                <Link to="/originals">
+                    <img src="/images/original-icon.svg" alt=""/>
+                    <span>ORIGINALS</span>
+                </Link>
+                <Link to="/movies">
+                    <img src="/images/movie-icon.svg" alt=""/>
+                    <span>MOVIES</span>
+                </Link>
+                <Link to="/series">
+                    <img src="/images/series-icon.svg" alt=""/>
+                    <span>SERIES</span>
+                </Link>
+            </NavMenu>
+            <Link to="/login" className="login">
+                LOGIN
+            </Link>
+        </Nav>
+    )
+}
+
+const Nav = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 70px;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 3;
+  background: #090b13;
+  
+  .login{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 8px 16px;
+    border: 2px solid #fff;
+    border-radius: 5px;
+    letter-spacing: 1.5px;
+    
+    &:hover{
+      background: #fff;
+      color: #000;
+    }
+  }
+`
+
+const Logo = styled.div`
+  width: 80px;
+
+  img {
+    width: 100%;
+  }
+`
+
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  flex-flow: row nowrap;
+  margin-left: 25px;
+  margin-right: auto;
+  
+  a{
+    position: relative;
+    display: flex;
+    
+    & + a{
+      margin-left: 20px;
+    }
+    
+    span{
+      padding-left: 5px;
+      letter-spacing: .13em;
+    }
+    
+    img{
+      width: 20px;
+    }
+  }
+  
+  @media (max-width: 768px){
+    display: none;
+  }
+`
+
+
+
+export default Header;
